4906100d8e0ea0d64070c7a395a726646cd5a518
Hennik Hunsaker <hennik@insidiouslogic.systems>
2023-04-11 21:31:22 -0600

    Microbox names and namespaces

--- boxfile_test.go
+++ boxfile_test.go
@@ -5,48 +5,46 @@ import "strings"
 import "encoding/json"
 import "fmt"
 
-
 func Testparse(t *testing.T) {
-  box := New([]byte("a: Easy!\nb:\n  c: 2\n  d: [3, 4]\n"))
-  if box.StringValue("a") != "Easy!" {
-    t.Error("boxfile parsed does not match boxfile in")
-  }
+	box := New([]byte("a: Easy!\nb:\n  c: 2\n  d: [3, 4]\n"))
+	if box.StringValue("a") != "Easy!" {
+		t.Error("boxfile parsed does not match boxfile in")
+	}
 
-  bad := New([]byte("baz:\n   cdr\nfoo::*)-> bar"))
-  if bad.Valid {
-    t.Error("Boxfile thinks its valid but it shoudnt be")
-  }
+	bad := New([]byte("baz:\n   cdr\nfoo::*)-> bar"))
+	if bad.Valid {
+		t.Error("Boxfile thinks its valid but it shoudnt be")
+	}
 
 }
 
-
 func TestNode(t *testing.T) {
-  box := New([]byte("web1:\n  name: site\n  type: php\n  version: 5.4\n  php_extensions:\n    - mysql\n    - gd\n    - eaccelerator\n"))
-  web1 := box.Node("web1")
-  if web1.StringValue("name") != "site" {
-    t.Error("nested nodes dont work")
-  }
-  if string(web1.raw) != "name: site\nphp_extensions:\n- mysql\n- gd\n- eaccelerator\ntype: php\nversion: 5.4\n" {
-    t.Error("subnodes dont create raw yaml correctly")
-  }
+	box := New([]byte("web1:\n  name: site\n  type: php\n  version: 5.4\n  php_extensions:\n    - mysql\n    - gd\n    - eaccelerator\n"))
+	web1 := box.Node("web1")
+	if web1.StringValue("name") != "site" {
+		t.Error("nested nodes dont work")
+	}
+	if string(web1.raw) != "name: site\nphp_extensions:\n- mysql\n- gd\n- eaccelerator\ntype: php\nversion: 5.4\n" {
+		t.Error("subnodes dont create raw yaml correctly")
+	}
 }
 
 func TestParsedSubParts(t *testing.T) {
-  box := New([]byte("a: Easy!\nb:\n  c: 2\n  d: [3, 4]\n"))
-  invalidNode := box.Node("nonya")
-  if invalidNode.Parsed == nil {
-    t.Error("the parsed data in a invalid node should be an empty map")
-  }
+	box := New([]byte("a: Easy!\nb:\n  c: 2\n  d: [3, 4]\n"))
+	invalidNode := box.Node("nonya")
+	if invalidNode.Parsed == nil {
+		t.Error("the parsed data in a invalid node should be an empty map")
+	}
 }
 
 func TestDeepNesting(t *testing.T) {
-  box := New([]byte(`run.config:
+	box := New([]byte(`run.config:
   engine: php
   engine.config:
     extensions:
       - mysqli
 data.mysql:
-  image: nanobox/mysql
+  image: mubox/mysql
   config:
     users:
     - username: root
@@ -55,11 +53,11 @@ data.mysql:
         - privilege: ALL PRIVILEGES
           'on': "*.*"
           with_grant: true
-    - username: nanobox
+    - username: microbox
       meta:
         privileges:
         - privilege: ALL PRIVILEGES
-          'on': gonano.*
+          'on': gomicro.*
           with_grant: true
         - privilege: ALL PRIVILEGES
           'on': testing.*
@@ -74,16 +72,16 @@ data.mysql:
           'on': "*.*"
           with_grant: false
         databases:
-        - gonano
+        - gomicro
         - testing
         - blah`))
 
-  b, err := json.Marshal(box.Node("data.mysql").Node("config").Parsed)
-  if err != nil {
-    t.Error("unable to marshal nested interfaces: %s", err)
-  }
-  fmt.Print(string(b))
-  if !strings.Contains(string(b), "blah.*") {
-    t.Error("does not contain data from deeply nested interfaces")
-  }
+	b, err := json.Marshal(box.Node("data.mysql").Node("config").Parsed)
+	if err != nil {
+		t.Error("unable to marshal nested interfaces: %s", err)
+	}
+	fmt.Print(string(b))
+	if !strings.Contains(string(b), "blah.*") {
+		t.Error("does not contain data from deeply nested interfaces")
+	}
 }
---
+++ go.mod
@@ -0,0 +1,7 @@
+module github.com/mu-box/microbox-boxfile
+
+go 1.20
+
+require launchpad.net/goyaml v0.0.0-20140305200416-000000000051
+
+require launchpad.net/gocheck v0.0.0-20140225173054-000000000087 // indirect
